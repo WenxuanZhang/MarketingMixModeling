bc41ce21c07753877aabd4cf93e03f45ad99af81
Wenxuan Zhang <ll.wxzhang@gmail.com>
2022-01-31 22:24:47 -0600

    coding started

--- MarketingMixModeling/app.R
+++ MarketingMixModeling/app.R
@@ -1,49 +1,63 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+################################################################################################################################
 
-library(shiny)
+wants <- c('tidyr','lubridate','tibble','data.table','knitr','caret','boot',
+           'ggplot2','gridExtra','pander','ggthemes','scales','foreign','magrittr','reshape2','glmnet',
+           'mgcv','cvTools','rpart','class','psych','stringr','rpart.plot',
+           'randomForest','corrplot','bit64','dplyr','readxl','openxlsx',
+           'stringr','shiny','shinydashboard','shinyWidgets','DT','rhandsontable','shinyMatrix',
+           'BBmisc','plotly','rlang','shinyalert','shinymanager')
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
+has <- wants %in% rownames(installed.packages())
+if(any(!has))install.packages(wants[!has])
+lapply(wants,library, character.only = T)
+
+inactivity <- "function idleTimer()
+var t = setTimeout(logout, 1200000);
+window.onmousemove = resetTimer;
+window.onmousedown = resetTimer;
+window.onclick = resetTimer;
+window.onscroll = resetTimer;
+window.onkeypress = resetTimer;
+
+function logout(){
+window.close();
+}
+
+function resetTimer(){
+clearTimeout(t)；
+t = setTimeout(logout,120000);
+}
+}
+idleTimer();
+"
+
+credentials <- data.frame(user = c("Wenxuan"),password = c("nikeyide"),
+                          stringsAsFactors = FALSE)
+
+
+
+ui<-secure_app(head_auth = tags$script(inactivity),
+               dashboardPage(skin = "green",
+                             dashboardHeader(title = "Marketing Mix Model"),
+                             dashboardSidebar(sidebarMenu(
+                                menuItem("Dashboard",tabName = "summary",icon = icon('list')),
+                                menuItem("Explore",tabName ="assort",icon = icon('th')),
+                                menuItem("Model",tabName ="optimization",icon = icon('signal')),
+                                menuItem("Optimization",tabName ="performance",icon = icon('search'))
+                             )),
+                             dashboardBody(tags$style(HTML('.content-wrapper, .right-side{
+                  background-color:#FFFFFF;
+                  }')))
+               ))
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+   result_auth <-secure_server(check_credentials = check_credentials(credentials))
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
 }
+
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
